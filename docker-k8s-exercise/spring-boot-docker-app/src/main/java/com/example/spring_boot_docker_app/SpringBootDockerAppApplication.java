6f6bfae99173a491dbbde3622dfea4260385d6c7
ildomota <ildo.mota@hotmail.com>
2026-03-12 08:39:47 +0100

    Initial DEVOPS exercises

---
+++ docker-k8s-exercise/spring-boot-docker-app/src/main/java/com/example/spring_boot_docker_app/SpringBootDockerAppApplication.java
@@ -0,0 +1,26 @@
+package com.example.spring_boot_docker_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerAppApplication {
+
+    @GetMapping("/")
+    public String index() {
+        return "<body bgcolor=\"#9FE2BF\">" +
+                "<h1>DevOps and Cloud</h1>" +
+                "<h2>Dockerizing and Deploying Spring Boot Application.</h2>" +
+                "<p>With docker, we can containerize SEP4 backend and frontend applications.</p>" +
+                "<p>We can also deploy the applications to <b>Kubernetes Cluster</b>.</p>" +
+                "</body>";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerAppApplication.class, args);
+    }
+}
