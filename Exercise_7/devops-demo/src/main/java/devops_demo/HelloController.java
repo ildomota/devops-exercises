56ce3b80080cd434fa0d8005724c15f095d7848f
ildomota <ildo.mota@hotmail.com>
2026-03-16 16:05:14 +0100

    ADD Exercise 7 - To be continued

---
+++ Exercise_7/devops-demo/src/main/java/devops_demo/HelloController.java
@@ -0,0 +1,14 @@
+package devops_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "DevOps Demo is running!";
+    }
+
+}
